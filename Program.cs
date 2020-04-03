8ed1b9532ad700ed500734d673573eed16c51dc0
Alexandru <alextauteanu99@gmail.com>
2020-04-03 09:42:34 +0300

    Proiect Baza Targ Auto

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Targ_Auto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string model = "";
+            string marca = "";
+            int _an = 0, _pret = 0, _culoare, _optiuni = 0;
+            const string nume_vanzator = "Alex";
+            //masina1
+            Console.WriteLine("Dati marca dorita: ");
+            marca = Console.ReadLine();
+            Console.WriteLine("Dati modelul dorit: ");
+            model = Console.ReadLine();
+            Console.WriteLine("Dati anul de fabricatie: ");
+            _an = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Dati pretul dorit: ");
+            _pret = Int32.Parse(Console.ReadLine()); ;
+            Console.WriteLine("Alegeti culoarea dorita: ");
+            Console.WriteLine("rosu = 1\n negru = 2\n galben = 3\n albastru = 4\n alb  = 5");
+            Int32.TryParse(Console.ReadLine(), out _culoare);
+
+            masini auto = new masini(marca, model, _an, _culoare, _pret);
+            Console.WriteLine("Alegeti optiuniile dorite: ");
+            do
+            {
+                Console.WriteLine("aer_conditionat = 1\nnavigatie = 2\ncutie_automata = 4\npachet_sline = 8\nexit = -1");
+                Int32.TryParse(Console.ReadLine(), out _optiuni);
+                if (_optiuni == 1 || _optiuni == 2 || _optiuni == 0 || _optiuni == 4 || _optiuni == 8)
+                    auto.Optiune |= _optiuni;
+            } while (_optiuni != -1);
+
+            Console.WriteLine("Introdu numele cumparatorului ");
+            auto.NumeCumparator = Console.ReadLine();
+            auto.NumeVanzator = nume_vanzator;
+            auto.DataTranzactiei = DateTime.Now.ToString();
+            Console.WriteLine(auto.Conversie_la_sir());
+            Console.ReadLine();
+
+            //masina2
+            Console.WriteLine("Dati marca dorita: ");
+            marca = Console.ReadLine();
+            Console.WriteLine("Dati modelul dorit: ");
+            model = Console.ReadLine();
+            Console.WriteLine("Dati anul de fabricatie: ");
+            _an = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Dati pretul dorit: ");
+            _pret = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Alegeti culoarea dorita: ");
+            Console.WriteLine("rosu = 1\n negru = 2\n galben = 3\n albastru = 4\n alb  = 5");
+            Int32.TryParse(Console.ReadLine(), out _culoare);
+            Console.WriteLine("Alegeti optiuniile dorite: ");
+            masini auto1 = new masini(marca, model, _an, _culoare, _pret);
+            do
+            {
+                Console.WriteLine("aer_conditionat = 1\nnavigatie = 2\ncutie_automata = 4\npachet_sline = 8\nexit = -1");
+                Int32.TryParse(Console.ReadLine(), out _optiuni);
+                if (_optiuni == 1 || _optiuni == 2 || _optiuni == 0 || _optiuni == 4 || _optiuni == 8)
+                    auto1.Optiune |= _optiuni;
+            } while (_optiuni != -1);
+
+            Console.WriteLine("Introdu numele cumparatorului ");
+            auto1.NumeCumparator = Console.ReadLine();
+            auto1.NumeVanzator = nume_vanzator;
+            auto1.DataTranzactiei = DateTime.Now.ToString();
+            Console.WriteLine(auto1.Conversie_la_sir());
+            Console.ReadLine();
+
+
+            switch (masini.Compara(auto, auto1))
+            {
+                case masini.LESS:
+                    Console.WriteLine("Prima masina este mai mica decat a doua");
+                    break;
+                case masini.MORE:
+                    Console.WriteLine("Prima masina este mai mare decat a doua");
+                    break;
+                case masini.EQUAL:
+                    Console.WriteLine("Prima masina este egala cu a doua");
+                    break;
+            }
+        }
+    }
+}
---
+++ masini.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targ_Auto
+{
+    class masini
+    {
+        public enum culoare
+        {
+            rosu = 1,
+            negru = 2,
+            galben = 3,
+            albastru = 4,
+            alb = 5
+        };
+
+
+        [Flags]
+        public enum optiuni
+        {
+            aer_conditionat = 1,
+            navigatie = 2,
+            cutie_automata = 4,
+            pachet_sline = 8,
+            nimic = 0
+        };
+
+        public const int LESS = -1;
+        public const int MORE = 1;
+        public const int EQUAL = 0;
+
+        public string NumeVanzator { get; set; }
+        public string NumeCumparator { get; set; }
+        public string Marca { get; set; }
+        public string Model { get; set; }
+        public int AnFabricatie { get; set; }
+        public int Culoare { get; set; }
+        public int Optiune { get; set; }
+        public string DataTranzactiei { get; set; }
+        public int Pret { get; set; }
+
+
+
+        public masini()
+        {
+            NumeCumparator = null;
+            Marca = null;
+            Model = null;
+            AnFabricatie = 0;
+            Culoare = 5;
+            Optiune = 0;
+            DataTranzactiei = null;
+            Pret = 0;
+        }
+
+
+
+        public masini(string Linie)
+        {
+            string[] separat = Linie.Split(',');
+            NumeVanzator = separat[0];
+            NumeCumparator = separat[1];
+            Marca = separat[2];
+            Model = separat[3];
+            AnFabricatie = int.Parse(separat[4]);
+            Culoare = int.Parse(separat[5]);
+            Optiune = int.Parse(separat[6]);
+            DataTranzactiei = separat[7];
+            Pret = int.Parse(separat[8]);
+        }
+
+        public masini(string marca, string model, int anFabricatie, int culoare, int pret)
+        {
+            Marca = marca;
+            Model = model;
+            AnFabricatie = anFabricatie;
+            Culoare = culoare;
+            Pret = pret;
+        }
+
+        public static int Compara(masini m1, masini m2)
+        {
+            return string.Compare(m1.Marca, m2.Marca);
+        }
+
+        public string Conversie_la_sir()
+        {
+            string t = "Automobilul are: \n";
+            t += "Vanzator: " + NumeVanzator + "\n";
+            t += "Cumparator: " + NumeCumparator + "\n";
+            t += "Data vanzare: " + DataTranzactiei + "\n";
+            t += "Marca: " + Marca + "\n";
+            t += "Modelul: " + Model + "\n";
+            t += "Culoarea: " + (culoare)Culoare + "\n";
+            t += "Pretul: " + Pret + "\n";
+            t += "Anul de fabricatie: " + AnFabricatie + "\n";
+            t += "Optiunile dumneavoastra sunt: \n";
+            foreach (int nr in Enum.GetValues(typeof(optiuni)))
+            {
+                if ((Optiune & nr) == nr)
+                    t += (optiuni)nr + "\n";
+            }
+            return t;
+        }
+    }
+}
